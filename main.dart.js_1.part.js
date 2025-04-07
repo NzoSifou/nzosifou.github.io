beac64ccf46618769c73e9fe26887c3d2b3bc693
NzoSifou <54418244+NzoSifou@users.noreply.github.com>
2025-04-07 17:23:59 +0000

    Deploying to gh-pages from @ NzoSifou/nzosifou.github.io@55b6045a642398837bdab8af71e8f663036166d6 🚀

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,36 @@
+((a,b,c)=>{a[b]=a[b]||{}
+a[b][c]=a[b][c]||[]
+a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
+akG(d,e,f){var y=new B.oA(C.jC),x=new A.J5(y)
+y.r=A.axS(x)
+return x},
+axQ(d,e){switch(e){case"about_me":return"About me"
+case"my_projets":return"My projects"
+case"my_skills":return"My skills"
+case"my_cv":return"My CV"
+default:return null}},
+axS(d){return new A.abL(d)},
+J5:function J5(d){this.a=d},
+abL:function abL(d){this.a=d}}
+B=c[0]
+C=c[2]
+A=a.updateHolder(c[3],A)
+A.J5.prototype={
+i(d,e){return this.a.B3(e)},
+gHy(){return"About me"},
+gKy(){return"My projects"},
+gKz(){return"My skills"},
+gKx(){return"My CV"},
+$iea:1,
+$ieM:1,
+gu3(){return this.a}}
+var z=a.updateTypes([])
+A.abL.prototype={
+$1(d){return A.axQ(this.a,d)},
+$S:42};(function inheritance(){var y=a.inherit
+y(A.J5,B.B)
+y(A.abL,B.hX)})()
+B.als(b.typeUniverse,JSON.parse('{"J5":{"ea":[],"eM":["dW","ea"]}}'))};
+((a,b)=>{a[b]=a.current
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"y+UC1GnSYFRnFj054anGsrvslHI=");
